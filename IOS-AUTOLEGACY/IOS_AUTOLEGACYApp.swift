8ba62493d2bd721b3031af6f469748b7da621955
chamuditha01 <heshanchamuditha05@gmail.com>
2026-04-05 23:05:58 +0530

    Initial Commit

---
+++ IOS-AUTOLEGACY/IOS_AUTOLEGACYApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_AUTOLEGACYApp.swift
+//  IOS-AUTOLEGACY
+//
+//  Created by Dileesha on 2026-04-05.
+//
+
+import SwiftUI
+
+@main
+struct IOS_AUTOLEGACYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
