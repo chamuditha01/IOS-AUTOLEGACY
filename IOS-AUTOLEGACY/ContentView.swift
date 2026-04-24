ef6f351ef8ab913b9638d1a8ebed7c39ef40da66
chamuditha01 <heshanchamuditha05@gmail.com>
2026-04-24 18:11:15 +0530

    Contet view update

--- IOS-AUTOLEGACY/ContentView.swift
+++ IOS-AUTOLEGACY/ContentView.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showLandingPage = true
+    @State private var showGetStartedPage = false
+    @State private var authScreen: AuthScreen = .login
+
+    private enum AuthScreen {
+        case login
+        case otp(phoneNumber: String)
+        case home
+    }
+    
     var body: some View {
-        Text("Checking connection...")
-            .task {
-                await checkConnection()
+        ZStack {
+            if showLandingPage {
+                LandingPageView {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        withAnimation(.easeInOut(duration: 0.5)) {
+                            showLandingPage = false
+                            showGetStartedPage = true
+                        }
+                    }
+                }
+            } else if showGetStartedPage {
+                GetStartedPageView {
+                    withAnimation(.easeInOut(duration: 0.4)) {
+                        showGetStartedPage = false
+                        authScreen = .login
+                    }
+                }
+            } else {
+                switch authScreen {
+                case .login:
+                    LoginView { phoneNumber in
+                        withAnimation(.easeInOut(duration: 0.3)) {
+                            authScreen = .otp(phoneNumber: phoneNumber)
+                        }
+                    }
+                case .otp(let phoneNumber):
+                    OTPVerificationView(phoneNumber: phoneNumber) {
+                        withAnimation(.easeInOut(duration: 0.3)) {
+                            authScreen = .home
+                        }
+                    }
+                case .home:
+                    HomeView()
+                }
             }
+        }
     }
 }
+
 #Preview {
     ContentView()
 }
